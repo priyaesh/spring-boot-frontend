398f22f286d78e135bf26c81820fcd671100d2b0
Hemapriya <hemapriyasampath@gmail.com>
2024-03-19 14:43:15 -0400

    List shopping cart items with empty cart

--- angular-ecommerce/src/app/services/cart.service.spec.ts
+++ angular-ecommerce/src/app/services/cart.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { CartService } from './CartService';
+import { CartService } from './cart.service';
 
 describe('CartService', () => {
   let service: CartService;
