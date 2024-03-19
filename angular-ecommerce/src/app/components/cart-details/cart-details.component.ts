978ec1aa32eebb4a99448b13e2e726374646e387
Hemapriya <hemapriyasampath@gmail.com>
2024-03-19 15:06:24 -0400

    Increment and decrement key in cartdetails

--- angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -37,5 +37,16 @@ export class CartDetailsComponent implements OnInit {
     // compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
+  }
 
 }
